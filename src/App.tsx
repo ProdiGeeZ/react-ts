de568110663f12d0e2e03bc862d94465ff7653c1
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-10-09 15:28:17 +0100

    Add ToDoList component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css';
+import ToDoList from './components/ToDoList';
 
 function App() {
+  const todos = [{ id: 't1', text: 'Finish this course.' }]
+
   return (
     <div className="App">
+      <ToDoList items={todos}/>
     </div>
   );
 }
---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface ToDoListProps {
+    items: { id: string, text: string }[];
+}
+
+const ToDoList: React.FC<ToDoListProps> = ({ items }) => {
+    return (
+        <ul>
+            {items.map(todo => <li key={todo.id}>{todo.text}</li>)}
+        </ul>
+    );
+}
+
+export default ToDoList;
